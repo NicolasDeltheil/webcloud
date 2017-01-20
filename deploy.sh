c5d1bf0bcaee5681973af0643faa99377f9972f7
NicolasDeltheil <nicolas.deltheil@orange.fr>
2017-01-20 10:29:37 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,28 @@
-#!/bin/bash 
-tail -n +3 "0"  root@138.68.64.15; exit 
-set -ev 
+#!/bin/bash
+	tail -n +3 "$0" | ssh ubuntu@138.68.64.15; exit;
 
-#update the server 
-apt-get update 
 
-apt-get install nginx
+	# Prevent errors
+	set -e
 
+
+	# Send apt-get update, upgrade, install ngnix
+	echo " ... apt-get update..."
+	sudo apt-get update
+	echo " ... apt-get upgrade..."
+	sudo apt-get upgrade
+	echo " ... apt-get install nginx..."
+	sudo apt-get install nginx
+
+
+	# Set permission to html folder
+	# chown ubuntu:ubuntu .
+	cd /var/www/html
+
+
+	# TODO: pull if exist, please
+  
+  # Cette commande permet à l'administrateur système d'accorder à certains utilisateurs la possibilité de lancer une commande en tant qu'administrateur, ou comme autre utilisateur. 
+  
+	sudo rm -Rf Webcloud
+	sudo git clone https://github.com/NicolasDeltheil/webcloud
